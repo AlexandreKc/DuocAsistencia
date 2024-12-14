2826ecf4098950ab51b663cf7788f6e90cc33c83
Sebastian <sebastiancarrera2021@gmail.com>
2024-12-14 17:33:10 -0300

    filtrado de cuentas por correo

--- src/app/medio/administrarcuentas/administrarcuentas.page.ts
+++ src/app/medio/administrarcuentas/administrarcuentas.page.ts
@@ -14,8 +14,9 @@ export class AdministrarcuentasPage implements OnInit {
   usuarios: any[] = []; // Aquí se almacenan los usuarios
 
   constructor(private databaseService: DatabaseService) {}
-
-  ngOnInit() {
+  busqueda: string = '';
+  usuariosFiltrados: any[] = []; 
+    ngOnInit() {
     this.cargarUsuarios();
   }
 
@@ -23,12 +24,24 @@ export class AdministrarcuentasPage implements OnInit {
     this.databaseService.getUsuarios().subscribe({
       next: (data) => {
         this.usuarios = data;
+        this.usuariosFiltrados = data; 
         console.log('Usuarios cargados:', this.usuarios);
       },
       error: (err) => {
         console.error('Error al cargar usuarios:', err);
       },
     });
+  }
+  filtrarUsuarios() {
+    if (this.busqueda.trim() === '') {
+      // Si no hay búsqueda, mostrar todos los usuarios
+      this.usuariosFiltrados = this.usuarios;
+    } else {
+      // Filtrar usuarios por correo
+      this.usuariosFiltrados = this.usuarios.filter((usuario) =>
+        usuario.correo.toLowerCase().includes(this.busqueda.toLowerCase())
+      );
+    }
   }
 }
 
